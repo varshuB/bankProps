e2d72c86b99b9091d9ecdc8a6e01281510c4d7cc
varshuB <varbejgam@gmail.com>
2024-04-18 14:02:07 +0530

    new structure of bank

--- bank/src/app/app-routing.module.ts
+++ bank/src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './Admin/Component/home/home.component';
-import { HeaderComponent } from './Admin/Component/header/header.component';
-import { FooterComponent } from './Admin/Component/footer/footer.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-  { path: 'header', component: HeaderComponent },
-  { path: 'footer', component: FooterComponent }
+  {
+    path: '',
+    redirectTo: 'pages',
+    pathMatch: 'full',
+  },
+  {
+    path: 'pages',
+    loadChildren: () => import('./pages/pages.module').then((m) => m.PagesModule),
+  },
 ];
 
 @NgModule({
