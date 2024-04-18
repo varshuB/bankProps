3af631763e1a8422b4ed3aad618186bcaeaa7649
BattulaMallika <mallikabattula57@gmail.com>
2024-04-18 16:00:10 +0530

    faq

--- bank/src/app/pages/pages.module.ts
+++ bank/src/app/pages/pages.module.ts
@@ -5,13 +5,17 @@ import { PagesRoutingModule } from './pages-routing.module';
 import { HomeComponent } from './components/home/home.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { MoreComponent } from './more/more.component';
+import { FaqComponent } from './components/faq/faq.component';
+import { ArticalOnIBAComponent } from './components/artical-on-iba/artical-on-iba.component';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
     ContactComponent,
-    MoreComponent
+    MoreComponent,
+    FaqComponent,
+    ArticalOnIBAComponent
   ],
   imports: [
     CommonModule,
