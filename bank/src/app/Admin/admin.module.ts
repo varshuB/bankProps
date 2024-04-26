d1cb68f4105ae71f6b007e5728597f6eeba1355f
BattulaMallika <mallikabattula57@gmail.com>
2024-04-26 18:05:31 +0530

    admin login changes

--- bank/src/app/Admin/admin.module.ts
+++ bank/src/app/Admin/admin.module.ts
@@ -28,6 +28,7 @@ import { UserListComponent } from './Component/pages/Users/UserList/user-list/us
     CommonModule,
     AdminRoutingModule
   ],
+  exports:[SignInComponent],
   schemas:[CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AdminModule { }
