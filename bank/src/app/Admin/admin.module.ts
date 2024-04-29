1d6d9c044563bba920b2aeda35524732fc20c6f2
Prashi022 <prashanthpitta0112@gmail.com>
2024-04-29 15:16:27 +0530

    user list, signup, recoverPw

--- bank/src/app/Admin/admin.module.ts
+++ bank/src/app/Admin/admin.module.ts
@@ -28,7 +28,7 @@ import { UserListComponent } from './Component/pages/Users/UserList/user-list/us
     CommonModule,
     AdminRoutingModule
   ],
-  exports:[SignInComponent],
+  exports:[SignInComponent, RecoverpwComponent,SignUpComponent,UserAddComponent,UserListComponent],
   schemas:[CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AdminModule { }
