d8ea606d0a3bdf4f6b4b996aab5852a8ef6dfef5
varshuB <varbejgam@gmail.com>
2024-05-02 10:58:30 +0530

    header changes

--- bank/src/app/layout/components/header/header.component.ts
+++ bank/src/app/layout/components/header/header.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Width } from 'ngx-owl-carousel-o/lib/services/carousel.service';
 import { RegisterFormComponent } from 'src/app/pages/components/register-form/register-form.component';
+import { MatDialog } from '@angular/material/dialog';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -8,16 +10,13 @@ import { RegisterFormComponent } from 'src/app/pages/components/register-form/re
 })
 export class HeaderComponent {
 
-  // constructor(private dialog:MatDialog){
-  constructor(){
+  constructor(private dialog:MatDialog){}
+
+  openPopUp(){
+    this.dialog.open(RegisterFormComponent, {
+      width:'500px',
+      height:'600px',
+    })
 
   }
-
-  // openPopUp(){
-  //   this.dialog.open(RegisterFormComponent, {
-  //     width:'500px',
-  //     height:'600px',
-  //   })
-
-  // }
 }
