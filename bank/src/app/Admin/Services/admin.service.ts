cf8d8d214b2c24ba2855fc0872fe9b6013038f73
varshuB <varbejgam@gmail.com>
2024-05-07 14:46:56 +0530

    admin service

--- bank/src/app/Admin/Services/admin.service.ts
+++ bank/src/app/Admin/Services/admin.service.ts
@@ -1,16 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminService {
 
-  mockUrl : string = "http://bankpropsservices.onlinedemo.co.in/v1/user/AddPropert";
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
-
-  addProperty(){
-
-    
+  addProperty(data:any){
+    return this.http.post(`${environment.apiUrl}/v1/user/AddProperty`, data)
   }
 }
